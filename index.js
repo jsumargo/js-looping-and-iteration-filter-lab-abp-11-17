ba30611604350a0aa87892bcd44324588b146f45
Jennifer Sumargo <jennifersumargo@yahoo.com>
2017-11-11 01:34:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Code your solution in this file
 function findMatching(drivers, string) {
+  console.log(drivers);
+  console.log(string);
   drivers.filter(function(driver) {return driver == string;});
 }

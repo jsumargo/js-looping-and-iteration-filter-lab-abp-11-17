27f8576c3bdaea4b686ae9e8e515bde506a6fc6f
Jennifer Sumargo <jennifersumargo@yahoo.com>
2017-11-11 02:29:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function findMatching(drivers, string) {
   return drivers.filter(function(driver){return driver.toLowerCase() === string.toLowerCase();});
 }
+
+function fuzzyMatch(drivers, string) {
+  
+}

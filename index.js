8733778683327d154c472edfb8fba964ca95d772
Jennifer Sumargo <jennifersumargo@yahoo.com>
2017-11-11 02:27:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
 function findMatching(drivers, string) {
-  return drivers.filter(function(driver){return driver === string;});
+  return drivers.filter(function(driver){return driver.toLowerCase() === string.toLowerCase();});
 }
